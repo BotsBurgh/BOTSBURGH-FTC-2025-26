1645d1a4e51fdfda37aa0f71a0300141ce7d51a8
Imthebiggestbird2 <joshbirendra@gmail.com>
2025-11-02 16:19:49 -0500

    trim: SpecterDriveTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SpecterDriveTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SpecterDriveTest.kt
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.autonomous
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import org.firstinspires.ftc.teamcode.api.CsvLogging
 import org.firstinspires.ftc.teamcode.api.TriWheels
 import org.firstinspires.ftc.teamcode.api.linear.SpecterDrive
 
@@ -10,29 +9,10 @@ import org.firstinspires.ftc.teamcode.api.linear.SpecterDrive
 
 class SpecterDriveTest: LinearOpMode() {
     override fun runOpMode() {
-        SpecterDrive.isLog = true
         SpecterDrive.init(this)
         TriWheels.init(this)
 
         waitForStart()
 
-        SpecterDrive.path(24.0, 0.0, 0.0, 5.0)
-        sleep(100)
-
-
-
-//        SpecterDrive.path(0.0, 24.0, 0.0, 5.0)
-//        sleep(100)
-//        SpecterDrive.path(-24.0, 0.0, 0.0, 5.0)
-//        sleep(100)
-//        SpecterDrive.path(0.0, -24.0, 0.0, 5.0)
-//        sleep(100)
-//        SpecterDrive.path(24.0, 24.0, 0.0, 5.0)
-//        sleep(100)
-//        SpecterDrive.path(-24.0, -24.0, 0.0, 5.0)
-//        sleep(100)
-
-        CsvLogging.close()
-        //SpecterDrive.path(-24.0,0.0,0.0,5.0)
     }
 }
