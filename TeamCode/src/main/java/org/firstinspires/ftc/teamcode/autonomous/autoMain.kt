90c4bb82b4148d3065aed06518065efb3c632726
Imthebiggestbird2 <joshbirendra@gmail.com>
2025-11-08 01:24:55 -0500

    add: shell autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autoMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autoMain.kt
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode.autonomous
 
-class autoMain {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.api.CsvLogging
+import org.firstinspires.ftc.teamcode.api.RobotTracker
+import org.firstinspires.ftc.teamcode.api.linear.SpecterDrive
+import org.firstinspires.ftc.teamcode.api.TriWheels
+
+@Autonomous(name = "autoMain")
+
+class autoMain : LinearOpMode(){
+
+    override fun runOpMode(){
+        TriWheels.init(this)
+        SpecterDrive.init(this)
+        RobotTracker.init(this)
+        CsvLogging.init(this)
+
+        waitForStart()
+
+        //SpecterDrive.linearPath(x,y,h)
+        //RobotTracker.addPos(x,y,h)
+        //do something
+
+
+        ///...end of auto
+        RobotTracker.logPos(0.0, 0.0)
+    }
 }
