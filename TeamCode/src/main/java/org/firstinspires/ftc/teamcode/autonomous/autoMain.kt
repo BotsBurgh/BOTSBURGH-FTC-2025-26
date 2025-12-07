7c3bd07ffebe7d3978f8b506e608c4448a27a2ff
revanth <revanthkkompala@gmail.com>
2025-12-07 12:14:34 -0500

    Changed to Some of Harish's settings: Turret; so power switch, correct controls, and speed correction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autoMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autoMain.kt
@@ -6,6 +6,8 @@ import org.firstinspires.ftc.teamcode.api.CsvLogging
 import org.firstinspires.ftc.teamcode.api.RobotTracker
 import org.firstinspires.ftc.teamcode.api.linear.SpecterDrive
 import org.firstinspires.ftc.teamcode.api.TriWheels
+import com.qualcomm.robotcore.hardware.DcMotor
+
 
 @Autonomous(name = "autoMain")
 
@@ -17,6 +19,28 @@ class autoMain : LinearOpMode(){
         RobotTracker.autoInit(this)
 
         waitForStart()
+
+
+        if (opModeIsActive()) {
+            TriWheels.green.mode = DcMotor.RunMode.RUN_USING_ENCODER
+            TriWheels.blue.mode = DcMotor.RunMode.RUN_USING_ENCODER
+            TriWheels.blue.power = -1.0     // small gentle push forward
+            TriWheels.green.power = 1.0    // small gentle push forward
+
+            sleep(5000)
+
+
+            TriWheels.blue.power = 0.0     // stop
+            TriWheels.green.power = 0.0     // stop
+
+            sleep(500)
+
+
+        }
+
+
+
+
     }
 
 }
