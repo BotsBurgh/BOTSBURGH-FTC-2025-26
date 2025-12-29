0f00d203b37c65826eade9effedeed74c7ef5ea6
revanth <revanthkkompala@gmail.com>
2025-12-29 15:24:24 -0500

    LL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Limelight.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Limelight.kt
@@ -1,43 +1,44 @@
 package org.firstinspires.ftc.teamcode.api
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.hardware.limelightvision.Limelight3A
+import org.firstinspires.ftc.teamcode.core.API
 
-object Limelight {
-
+object Limelight: API(){
+//cam is the variable for the limelight
     private lateinit var cam: Limelight3A
-
+    //this is the target ID to change the pipeline to the ID for the limelight to give results on
     private var targetId: Int = -1
-
+// the angle given by the limelight
     var angleX = 0.0
         private set
-
+    //A check to make sure the april tag is visible
     var seesTag = false
         private set
 
-    fun init(opMode: OpMode) {
+    override fun init(opMode: OpMode) {
         cam = opMode.hardwareMap.get(Limelight3A::class.java, "limelight")
 
-        // Start on default pipeline
+//the default limelight to target april tag
         cam.pipelineSwitch(0)
         cam.start()
     }
 
     fun update() {
-        val result = cam.latestResult
 
+        val result = cam.latestResult
+//this is a measure to make sure it doesn't provide a value when nothiing is visible
         if (result == null || !result.isValid) {
             seesTag = false
             angleX = 0.0
             return
         }
-
+//target ID
         val tag = if (targetId == -1) {
             result.fiducialResults.firstOrNull()
         } else {
             result.fiducialResults.find { it.fiducialId == targetId }
         }
-
+//print the value
         if (tag != null) {
             seesTag = true
             angleX = tag.targetXDegrees
@@ -47,7 +48,7 @@ object Limelight {
         }
     }
 
-
+//change the target ID
     fun changeTagID(id: Int) {
         targetId = id
 
@@ -56,5 +57,8 @@ object Limelight {
         } else {
             cam.pipelineSwitch(0)
         }
+    fun limelightInfo(){
+
+    }
     }
 }
